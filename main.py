06253c6260e04c2635432ed85624cf5b7e3e902a
Charlie Welland <charliewelland@Charlies-MacBook-Pro.local>
2023-04-18 01:17:22 -0700

    main

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pandas as pd
 import requests
 import plotly.express as px
 
+
+
 def generate_data() -> List[int]:
     """Generate some random data."""
     return np.random.randint(100, 10000, 1000).tolist()
@@ -35,7 +37,7 @@ def final_process(data1: List[int], data2: List[int]) -> List[int]:
     """TODO: Document this function. What does it do? What are the inputs and outputs?"""
     return np.mean([x - y for x, y in zip(data1, data2)])
 
-offload_url = 'http://192.168.4.74:5000' # TODO: Change this to the IP address of your server
+offload_url = 'http://127.0.0.1:5000/' # TODO: Change this to the IP address of your server
 
 def run(offload: Optional[str] = None) -> float:
     """Run the program, offloading the specified function(s) to the server.
@@ -55,6 +57,7 @@ def run(offload: Optional[str] = None) -> float:
         def offload_process1(data):
             nonlocal data1
             # TODO: Send a POST request to the server with the input data
+            response = requests.post(f"{offload_url}/process1",json=data)
             data1 = response.json()
         thread = threading.Thread(target=offload_process1, args=(data,))
         thread.start()
@@ -67,10 +70,36 @@ def run(offload: Optional[str] = None) -> float:
         #   Make sure to cite any sources you use to answer this question.
     elif offload == 'process2':
         # TODO: Implement this case
-        pass
+        data2 = None
+        def offload_process2(data):
+            nonlocal data2
+            # TODO: Send a POST request to the server with the input data
+            response = requests.post(f"{offload_url}/process2",json=data)
+            data2 = response.json()
+        thread = threading.Thread(target=offload_process2, args=(data,))
+        thread.start()
+        data1 = process1(data)
+        thread.join()
     elif offload == 'both':
         # TODO: Implement this case
-        pass
+        data1 = None
+        def offload_process1(data):
+            nonlocal data1
+            # TODO: Send a POST request to the server with the input data
+            response1 = requests.post(f"{offload_url}/process1",json=data)
+            data1 = response1.json()
+        thread1 = threading.Thread(target=offload_process1, args=(data,))
+        thread1.start()
+        thread1.join()
+        data2 = None
+        def offload_process2(data):
+            nonlocal data2
+            # TODO: Send a POST request to the server with the input data
+            response2 = requests.post(f"{offload_url}/process2",json=data)
+            data2 = response2.json()
+        thread2 = threading.Thread(target=offload_process2, args=(data,))
+        thread2.start()
+        thread2.join()
 
     ans = final_process(data1, data2)
     return ans 
@@ -79,13 +108,57 @@ def main():
     # TODO: Run the program 5 times for each offloading mode, and record the total execution time
     #   Compute the mean and standard deviation of the execution times
     #   Hint: store the results in a pandas DataFrame, use previous labs as a reference
-
+    offload_modes = ['process1', 'process2', 'both']
+    execution_times = []
+    mean_times = []
+    std_times = []
+    for i in range(5):
+        start_time = time.time()
+        run()
+        end_time = time.time()
+        execution_time = end_time - start_time
+        execution_times.append(execution_time)
+    mean_execution_time = np.mean(execution_times)
+    mean_times.append(mean_execution_time)
+    std_execution_time = np.std(execution_times)
+    std_times.append(std_execution_time)
+    for mode in offload_modes:
+        for i in range(5):
+            start_time = time.time()
+            run(mode)
+            end_time = time.time()
+            execution_time = end_time - start_time
+            execution_times.append(execution_time)
+        mean_execution_time = np.mean(execution_times)
+        mean_times.append(mean_execution_time)
+        std_execution_time = np.std(execution_times)
+        std_times.append(std_execution_time)
 
     # TODO: Plot makespans (total execution time) as a bar chart with error bars
     # Make sure to include a title and x and y labels
 
-
     # TODO: save plot to "makespan.png"
+
+
+
+
+    # Sample data
+    modes = ['None', 'Process 1', 'Process 2', 'Both']
+
+    # Create a DataFrame from the data
+    data = {'Mode': modes, 'Makespan': mean_times, 'Error': std_times}
+    df = pd.DataFrame(data)
+
+    # Create a bar chart with error bars using Plotly Express (px)
+    fig = px.bar(df, x='Mode', y='Makespan', error_y='Error', 
+                 error_y_minus='Error',  # Optional, specify if the error is symmetric
+                 labels={'Makespan': 'Total Execution Time'},
+                 title='Total Execution Time with Error Bars')
+
+    # Show the chart
+    fig.show()
+    fig.write_image("makespan.png")
+
 
 
     # Question 4: What is the best offloading mode? Why do you think that is?
